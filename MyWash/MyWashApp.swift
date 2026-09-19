37ed9127690eb7c851c10808b616a98b85d43bce
Cody <simple.evander@gmail.com>
2026-09-19 12:51:22 -0500

    Initial Commit

---
+++ MyWash/MyWashApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyWashApp.swift
+//  MyWash
+//
+//  Created by Cody Evander on 9/19/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyWashApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
